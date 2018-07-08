0159370a8044d8a23eb44622a576b36c1526d1f1
jagad <jagad@DESKTOP-AL9CEI8>
2018-07-08 16:23:30 +0530

    added

---
+++ DevlopAP/src/main/java/com/example/demo/DevlopApApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class DevlopApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevlopApApplication.class, args);
+		
+	}
+	
+
+	@RequestMapping(path = "/hh",method=RequestMethod.GET)
+	public String wishMsg(){
+		return "Hello world";
+	}
+	
+	
+	
+}
